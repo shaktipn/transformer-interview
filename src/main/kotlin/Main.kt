b3dfa229e8ea9c87ed2e5908425c7335ca03849d
shaktipn <shakti.pn@surya-digital.com>
2024-06-07 19:52:14 +0530

    Update doc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,17 @@ package org.example
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import org.example.subcommands.Clean
+import org.example.subcommands.Convert
 
 fun main(args: Array<String>) {
     Transformer().subcommands (
-        Clean()
+        Clean(),
+        Convert()
     ).main(args)
 }
 
 class Transformer: CliktCommand(help = "transformer commands", name = "transformer") {
     override fun run() {
-        echo("hi")
+        echo("Executing command...")
     }
 }
